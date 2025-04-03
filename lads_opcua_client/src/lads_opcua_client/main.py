83f4ebabc5b5b2e5b58f5e60db868ad44deb87d6
Matthias Arnold <42539387+DrMatthiasArnold@users.noreply.github.com>
2025-04-03 18:17:27 +0200

    slightly increase robustness of LADS client

--- lads_opcua_client/src/lads_opcua_client/main.py
+++ lads_opcua_client/src/lads_opcua_client/main.py
@@ -1290,7 +1290,10 @@ class Function(LADSNode):
     
     @property
     def variables(self) ->list[BaseVariable]:
-        return [self.is_enabled]
+        if (self.is_enabled is None):
+            return []
+        else:
+            return [self.is_enabled]
     
     @property
     def all_variables(self) -> list[BaseVariable]:
